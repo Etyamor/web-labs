fed304f8f76815445e72430975c27cb60f053fce
Max Rutk <maxikrud0071@gmail.com>
2018-06-04 10:06:14 +0300

    jquery fix

--- js/jq.js
+++ js/jq.js
@@ -2,7 +2,7 @@ jQuery('#myInput').keyup(function() {
     jQuery('#charCount').text( this.value.replace(/{.*}/g, '').length );
 });
 
-jQuery( document ).ready(function() {
+jQuery( document ).ready(function($) {
     
 	$('.burger').click(function(){
 		$('header ul').toggleClass('opened');
